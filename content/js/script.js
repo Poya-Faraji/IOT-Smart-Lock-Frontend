e1d920b98b4170d4c0ab5b8a51fc5b42c1c6d351
Poya Faraji <poya.faraji@outlook.com>
2024-12-31 01:04:20 +0330

    Add Cooldown for button clicks
    Description
    - Removed ESP32 Request Simulation
    - Changed Styles for Button Disabled mode
    - Buttons have cooldown after click to avoid request spam to the server.

--- content/js/script.js
+++ content/js/script.js
@@ -1,12 +1,15 @@
 const unlockBtnElem = document.getElementById("unlock-btn");
 const cancelBtnElem = document.getElementById("cancel-btn");
-const statusTextElem = document.getElementById("lock-status");
+// const statusTextElem = document.getElementById("lock-status");
 
-
-const API_URL = 'https://smart-iot-lock.onrender.com'
+const API_URL = "https://smart-iot-lock.onrender.com";
 
 unlockBtnElem.addEventListener("click", () => {
   unlock();
+  unlockBtnElem.setAttribute("disabled", "disabled");
+  setTimeout(() => {
+    unlockBtnElem.removeAttribute("disabled");
+  }, 3000);
 });
 
 const unlock = async () => {
@@ -27,21 +30,25 @@ const reset = async () => {
     .catch((error) => console.error("Error:", error));
 };
 
-
-const lockSimulation = async () => {
-  await fetch(`${API_URL}/api/unlock/`, { method: "GET" })
-    .then((response) => response.text())
-    .then((data) => {
-      statusTextElem.innerText = `Unlocked : ${data}`
-    })
-    .catch((error) => console.error("Error:", error));
-};
+// const lockSimulation = async () => {
+//   await fetch(`${API_URL}/api/unlock/`, { method: "GET" })
+//     .then((response) => response.text())
+//     .then((data) => {
+//       statusTextElem.innerText = `Unlocked : ${data}`;
+//     })
+//     .catch((error) => console.error("Error:", error));
+// };
 
 cancelBtnElem.addEventListener("click", () => {
   reset();
+  cancelBtnElem.setAttribute("disabled", "disabled");
+  setTimeout(() => {
+    cancelBtnElem.removeAttribute("disabled");
+  }, 3000);
 });
 
+// Simulation Codes
 
-setInterval(() => {
-  lockSimulation()
-}, 3000);
+// setInterval(() => {
+//   lockSimulation();
+// }, 3000);
